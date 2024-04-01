123fe348214619f6c836b16d4cf196436f33227f
rafaelbdmelo117 <rafael_b_de_melo@hotmail.com>
2024-04-01 19:11:55 -0300

    calcula_figura was upgraded. It now has a working perimeter calculator for circles as well as functions for scanning Ints and Doubles

--- lista_aula_3/calcula_figura.java
+++ lista_aula_3/calcula_figura.java
@@ -4,32 +4,39 @@ import java.lang.Math;
 
 public class calcula_figura{
 
+    //funcao para escanear inteiros
+    public static int scanInt(Scanner scI) {
+        return Integer.parseInt(scI.nextLine());
+    }
+
+    //funcao para escanear double
+    public static double scanDouble(Scanner scD) {
+        return Double.parseDouble(scD.nextLine());
+    }
+
+    //funcao para calcular perimetro do circulo
     public static double calculaPerimetroCirculo(double radius){
         double perimetroCirculo;
         perimetroCirculo = 2 * Math.PI * radius;
         return perimetroCirculo;
     }
+
+    //funcao main
     public static void main(String[] args){
         //var para selecionar a figura geometrica
         int tipoFigura;
 
-        //objeto Scanner para receber o tipo de figura
-        Scanner recebeFigura = new Scanner(System.in);
+        //objeto Scanner
+        Scanner entrada = new Scanner(System.in);
 
         //faz a leitura do tipo da figura
         System.out.print("Você deseja calcular o perímetro e a área de qual figura?\n" + "(1) Círculo\n" + "(2) Quadrado\n" + "(3) Retângulo\n" + "(4) Trapézio\n");
-        tipoFigura = recebeFigura.nextInt();
-
-        //fecha o scanner das figuras
-        //recebeFigura.close();
+        tipoFigura = scanInt(entrada);
 
         //var para o resultado do perimetro
         double resultadoPerimetro;
         //var para o resultado da area
         //double resultadoArea;
-
-        //Scanner para receber as medidas das figuras
-        //Scanner recebeMedidas = new Scanner(System.in);
 
         switch (tipoFigura) {
             case 1:
@@ -37,14 +44,14 @@ public class calcula_figura{
                 double raio;
 
                 //solicita a entrada do raio ao usuario
-                System.out.print("Você selecionou círculo. Digite o raio do círculo: ");
-                raio = recebeFigura.nextDouble();
+                System.out.print("Você selecionou círculo.\nDigite o raio do círculo: ");
+                raio = scanDouble(entrada);
                 
                 //joga o valor na funcao e chama a funcao perimetro
                 resultadoPerimetro = calculaPerimetroCirculo(raio);
 
                 //mostra o resultado do perimetro
-                System.out.println("O perímetro do círculo é: " + resultadoPerimetro);
+                System.out.printf("O perímetro do círculo é: " + "%,.2f", resultadoPerimetro);
 
                 //resultadoArea = calculaAreaCirculo(raio);
                 //System.out.println("A área do círculo é: " + resultadoArea);
@@ -61,6 +68,5 @@ public class calcula_figura{
                 System.out.println("Esse valor não corresponde a nenhuma opção.");
                 break;
         }
-        //recebeMedidas.close();
     }
 }
