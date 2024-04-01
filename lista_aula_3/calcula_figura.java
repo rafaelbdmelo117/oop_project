98f3628abd1b900f2b11ebacea3b79c0eefdfd43
rafaelbdmelo117 <rafael_b_de_melo@hotmail.com>
2024-04-01 19:36:14 -0300

    Now this program has an functional square area and perimeter calculator. I have done some minor changes to the code as well.

--- lista_aula_3/calcula_figura.java
+++ lista_aula_3/calcula_figura.java
@@ -15,10 +15,27 @@ public class calcula_figura{
     }
 
     //funcao para calcular perimetro do circulo
-    public static double calculaPerimetroCirculo(double radius){
-        double perimetroCirculo;
-        perimetroCirculo = 2 * Math.PI * radius;
+    public static double calculaPerimetroCirculo(double r){
+        double perimetroCirculo = 2 * Math.PI * r;
         return perimetroCirculo;
+    }
+
+    //funcao para calcular area do circulo
+    public static double calculaAreaCirculo(double r){
+        double areaCirculo = Math.PI * Math.pow(r, 2);
+        return areaCirculo;
+    }
+
+    //funcao para calcular perimetro do quadrado
+    public static double calculaPerimetroQuadrado(double l){
+        double perimetroQuadrado = 4 * l;
+        return perimetroQuadrado;
+    }
+
+    //funcao para calcular area do quadrado
+    public static double calculaAreaQuadrado(double l){
+        double areaQuadrado = l * l;
+        return areaQuadrado;
     }
 
     //funcao main
@@ -36,7 +53,7 @@ public class calcula_figura{
         //var para o resultado do perimetro
         double resultadoPerimetro;
         //var para o resultado da area
-        //double resultadoArea;
+        double resultadoArea;
 
         switch (tipoFigura) {
             case 1:
@@ -47,17 +64,37 @@ public class calcula_figura{
                 System.out.print("Você selecionou círculo.\nDigite o raio do círculo: ");
                 raio = scanDouble(entrada);
                 
-                //joga o valor na funcao e chama a funcao perimetro
+                //joga o valor na funcao perimetro
                 resultadoPerimetro = calculaPerimetroCirculo(raio);
+                //joga o valor na funcao area
+                resultadoArea = calculaAreaCirculo(raio);
 
                 //mostra o resultado do perimetro
                 System.out.printf("O perímetro do círculo é: " + "%,.2f", resultadoPerimetro);
-
-                //resultadoArea = calculaAreaCirculo(raio);
-                //System.out.println("A área do círculo é: " + resultadoArea);
+                System.out.printf("\n");
+                //mostra o resultado da area
+                System.out.printf("A área do círculo é: " + "%,.2f", resultadoArea);
+                System.out.printf("\n");
                 break;
             case 2:
-                //funcao quadrado
+                //var para o lado do quadrado
+                double lado;
+
+                //solicita a entrada do lado ao usuario
+                System.out.print("Você selecionou quadrado.\nDigite o tamanho do lado do quadrado: ");
+                lado = scanDouble(entrada);
+
+                //joga o valor na funcao perimetro
+                resultadoPerimetro = calculaPerimetroQuadrado(lado);
+                //joga o valor na funcao area
+                resultadoArea = calculaAreaQuadrado(lado);
+
+                //mostra o resultado do perimetro
+                System.out.printf("O perímetro do quadrado é: " + "%,.2f", resultadoPerimetro);
+                System.out.printf("\n");
+                //mostra o resultado da area
+                System.out.printf("A área do quadrado é: " + "%,.2f", resultadoArea);
+                System.out.printf("\n");
                 break;
             case 3:
                 //funcao retangulo
