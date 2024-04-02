8f24d6fecfdfc3b865f09509b1cfebfb3e585517
rafaelbdmelo117 <rafael_b_de_melo@hotmail.com>
2024-04-02 16:17:43 -0300

    Triangle area and perimeter calculator implemented. I should do a hypotenuse calculator too.

--- lista_aula_3/calcula_figura.java
+++ lista_aula_3/calcula_figura.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 import java.lang.Math;
 
 public class calcula_figura{
+    //var para verificacao de condicoes
+    public static boolean ok;
 
     //funcao para escanear inteiros
     public static int scanInt(Scanner scI) {
@@ -16,7 +18,7 @@ public class calcula_figura{
 
     //funcao para calcular perimetro do circulo
     public static double calculaPerimetroCirculo(double r){
-        double perimetroCirculo = 2 * Math.PI * r;
+        double perimetroCirculo = 2 * Math.PI * Math.abs(r);
         return perimetroCirculo;
     }
 
@@ -28,7 +30,7 @@ public class calcula_figura{
 
     //funcao para calcular perimetro do quadrado
     public static double calculaPerimetroQuadrado(double l){
-        double perimetroQuadrado = 4 * l;
+        double perimetroQuadrado = 4 * Math.abs(l);
         return perimetroQuadrado;
     }
 
@@ -40,26 +42,66 @@ public class calcula_figura{
 
     //funcao para calcular perimetro do retangulo
     public static double calculaPerimetroRetangulo(double x, double y){
-        double perimetroRetangulo = (2 * x) + (2 * y);
+        double perimetroRetangulo = (2 * Math.abs(x)) + (2 * Math.abs(y));
         return perimetroRetangulo;
     }
 
     //funcao para calcular area do retangulo
     public static double calculaAreaRetangulo(double x, double y){
-        double areaRetangulo = x * y;
+        double areaRetangulo = Math.abs(x) * Math.abs(y);
         return areaRetangulo;
     }
 
     //funcao para calcular perimetro do trapezio
     public static double calculaPerimetroTrapezio(double b, double B, double l1, double l2){
-        double perimetroTrapezio = b + B + l1 + l2;
+        double perimetroTrapezio = Math.abs(b) + Math.abs(B) + Math.abs(l1) + Math.abs(l2);
         return perimetroTrapezio;
     }
 
     //funcao para calcular area do trapezio
     public static double calculaAreaTrapezio(double b, double B, double h){
-        double areaTrapezio = ((b + B) * h) / 2;
+        double areaTrapezio = ((Math.abs(b) + Math.abs(B)) * Math.abs(h)) / 2;
         return areaTrapezio;
+    }
+
+    //funcao verifica triangulo
+    public static boolean verificaTriangulo(double a, double b, double c){
+        //essa secao testa as condicoes de existencia do triangulo
+        if(Math.abs(a) < Math.abs(b + c) && Math.abs(b - c) < Math.abs(a)){
+            if(Math.abs(b) < Math.abs(a + c) && Math.abs(a - c) < Math.abs(b)){
+                if(Math.abs(c) < Math.abs(a + b) && Math.abs(a - b) < Math.abs(c)){
+                    ok = true;
+                }else{
+                    ok = false;
+                }
+            }else{
+                ok = false;
+            }
+        }else{
+            ok = false;
+        }
+        return ok;
+    }
+
+    //funcao para calcular perimetro do triangulo
+    public static double calculaPerimetroTriangulo(double a, double b, double c){
+        //verifica se as condicoes estao satisfeitas e calcula
+        if(ok == true){
+            double perimetroTriangulo = Math.abs(a) + Math.abs(b) + Math.abs(c);
+            return perimetroTriangulo;
+        }else{
+            return 0;
+        }
+    }
+
+    //funcao para calcular area do triangulo
+    public static double calculaAreaTriangulo(double l, double h){
+        if(ok == true){
+            double areaTriangulo = (Math.abs(l) * Math.abs(h)) / 2;
+            return areaTriangulo;
+        }else{
+            return 0;
+        }
     }
 
     //funcao main
@@ -71,7 +113,7 @@ public class calcula_figura{
         Scanner entrada = new Scanner(System.in);
 
         //faz a leitura do tipo da figura
-        System.out.print("Você deseja calcular o perímetro e a área de qual figura?\n" + "(1) Círculo\n" + "(2) Quadrado\n" + "(3) Retângulo\n" + "(4) Trapézio\n");
+        System.out.print("As opções são:\n" + "(1) Círculo\n" + "(2) Quadrado\n" + "(3) Retângulo\n" + "(4) Trapézio\n" + "(5) Triângulo\n" + "Faça sua escolha: ");
         tipoFigura = scanInt(entrada);
 
         //var para o resultado do perimetro
@@ -155,9 +197,9 @@ public class calcula_figura{
                 bM = scanDouble(entrada);
                 System.out.print("Agora, informe a medida da altura do trapézio: ");
                 h = scanDouble(entrada);
-                System.out.print("Diga também a medida da lateral esquerda do trapézio: ");
+                System.out.print("Informe também a medida da lateral esquerda do trapézio: ");
                 l1 = scanDouble(entrada);
-                System.out.print("Por fim, digite a medida da lateral direita do trapézio: ");
+                System.out.print("Por fim, informe a medida da lateral direita do trapézio: ");
                 l2 = scanDouble(entrada);
 
                 //joga os valores na funcao perimetro
@@ -172,8 +214,42 @@ public class calcula_figura{
                 System.out.printf("A área do trapézio é: " + "%,.2f", resultadoArea);
                 System.out.printf("\n");
                 break;
+            case 5:
+                //var para verificacao
+                boolean confirma;
+                //vars para receber as medidas do triangulo
+                double l, ht, c;
+
+                //solicita a entrada de dados do triangulo
+                System.out.print("Você selecionou triângulo.\n");
+                System.out.print("Informe a medida da base do triângulo ou de um dos catetos: ");
+                l = scanDouble(entrada);
+                System.out.print("Informe a medida da altura do triângulo ou do outro cateto: ");
+                ht = scanDouble(entrada);
+                System.out.print("Informe a medida do lado restante do triângulo ou da hipotenusa: ");
+                c = scanDouble(entrada);
+
+                //joga os valores na funcao verificadora
+                confirma = verificaTriangulo(l, ht, c);
+                if(confirma){
+                    //joga os valores na funcao perimetro
+                    resultadoPerimetro = calculaPerimetroTriangulo(l, ht, c);
+                    //joga os valores na funcao area
+                    resultadoArea = calculaAreaTriangulo(l, ht);
+
+                    //mostra o resultado do perimetro
+                    System.out.printf("O perímetro do triângulo é: " + "%,.2f", resultadoPerimetro);
+                    System.out.printf("\n");
+                    //mostra o resultado da area
+                    System.out.printf("A área do triângulo é: " + "%,.2f", resultadoArea);
+                    System.out.printf("\n");
+                }else{
+                    //informa que os valores nao funcionam
+                    System.out.println("Desculpe. Os valores que você informou não são razoáveis.");
+                }
+                break;
             default:
-                System.out.println("Esse valor não corresponde a nenhuma opção.");
+                System.out.println("Essa não é uma opção válida. Leia com calma e tente novamente.");
                 break;
         }
     }
